c2c523eb46c2a164078b6d18ed8f7ff5baecdfce
Jakub Kropáček <kropikuba@gmail.com>
2024-03-30 13:43:02 +0100

    fixx

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,57 @@
 <template>
-  <h1>JustScreaMy</h1>
-  <p>Hello! My name is Jakub Kropáček and I&apos;m from Czechia.</p>
-  <p>
-    I have been interested in programming since my primary school, after I saw
-    a Scratch icon on my desktop in computer science class. That didn&apos;t
-    last long, after few days I found out about JavaScript on Youtube. And
-    that is why I chose the path I&apos;m going on right now.
-  </p>
-  <Separator/>
-  <p>
-    I chose
-    <NuxtLink to="https://www.spseiostrava.cz/en/" target="_blank">
-      Technical School of Electrical Engineering and Computer Science
-    </NuxtLink>
-    as my secondary school, and continued to
-    <NuxtLink to="https://www.fei.vsb.cz/en" target="_blank">
-      VSB - Technical University of Ostrava
-    </NuxtLink>
-    as my college (which im still studying right now).
-  </p>
-  <Separator/>
-  <p>
-    I&apos;m mostly interested in Networking Technologies and Operating Systems
-    but that is only a plan for a university in my future.
-  </p>
-  <Separator/>
-  <p>
-    What I do right now is experiment with operating systems and configure and deploy services.
-    I also love learning and trying new technologies
-  </p>
-  <Separator/>
-  <p>
-    Here are some languages I have tried and have found that I can work decently well with:
-  </p>
-  <ul>
-    <li>Python</li>
-    <li>JavaScript</li>
-    <li>C</li>
-    <li>PHP (Little more knowledge than Go)</li>
-    <li>HTML/CSS (I don&apos;t like CSS tho)</li>
-    <li>SQL</li>
-  </ul>
-  <p>And there are my other skills and knowledge:</p>
-  <ul>
-    <li>Linux</li>
-    <li>Git</li>
-    <li>Gitlab CI</li>
-    <li>Docker</li>
-    <li>Cisco IOS</li>
-  </ul>
+  <div>
+
+    <h1>JustScreaMy</h1>
+    <p>Hello! My name is Jakub Kropáček and I&apos;m from Czechia.</p>
+    <p>
+      I have been interested in programming since my primary school, after I saw
+      a Scratch icon on my desktop in computer science class. That didn&apos;t
+      last long, after few days I found out about JavaScript on Youtube. And
+      that is why I chose the path I&apos;m going on right now.
+    </p>
+    <Separator/>
+    <p>
+      I chose
+      <NuxtLink to="https://www.spseiostrava.cz/en/" target="_blank">
+        Technical School of Electrical Engineering and Computer Science
+      </NuxtLink>
+      as my secondary school, and continued to
+      <NuxtLink to="https://www.fei.vsb.cz/en" target="_blank">
+        VSB - Technical University of Ostrava
+      </NuxtLink>
+      as my college (which im still studying right now).
+    </p>
+    <Separator/>
+    <p>
+      I&apos;m mostly interested in Networking Technologies and Operating Systems
+      but that is only a plan for a university in my future.
+    </p>
+    <Separator/>
+    <p>
+      What I do right now is experiment with operating systems and configure and deploy services.
+      I also love learning and trying new technologies
+    </p>
+    <Separator/>
+    <p>
+      Here are some languages I have tried and have found that I can work decently well with:
+    </p>
+    <ul>
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>C</li>
+      <li>PHP (Little more knowledge than Go)</li>
+      <li>HTML/CSS (I don&apos;t like CSS tho)</li>
+      <li>SQL</li>
+    </ul>
+    <p>And there are my other skills and knowledge:</p>
+    <ul>
+      <li>Linux</li>
+      <li>Git</li>
+      <li>Gitlab CI</li>
+      <li>Docker</li>
+      <li>Cisco IOS</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
--- pages/repos.vue
+++ pages/repos.vue
@@ -1,20 +1,22 @@
-<template>
-  <template v-if="pending"> <p>Loading...</p> </template>
-  <template v-else-if="error">
-    <p class="error">Failed to load, please try again later.</p>
-  </template>
-  <template v-else v-for="repository in data" :key="repository.id">
-    <Repository v-if="!repository.fork" :repository />
-  </template>
-</template>
-
 <script lang="ts" setup>
 import type {Repository} from "~/types/github";
 
-const { data, error, pending } = await useLazyFetch<Array<Repository>>(
-  "https://api.github.com/users/justscreamy/repos"
+const {data, error, pending} = await useLazyFetch<Array<Repository>>(
+    "https://api.github.com/users/justscreamy/repos"
 );
 </script>
+
+<template>
+  <div>
+    <template v-if="pending"><p>Loading...</p></template>
+    <template v-else-if="error">
+      <p class="error">Failed to load, please try again later.</p>
+    </template>
+    <template v-else v-for="repository in data" :key="repository.id">
+      <Repository v-if="!repository.fork" :repository/>
+    </template>
+  </div>
+</template>
 
 <style>
 .error {
