a63baf39675957d5fcf5ad769ecfbc2124a7baff
Jakub Kropáček <kropikuba@gmail.com>
2024-03-30 13:26:06 +0100

    NuxtImage params change

--- components/Repository.vue
+++ components/Repository.vue
@@ -3,7 +3,7 @@
     <h2>{{ props.repository.name }}</h2>
     <p>{{ props.repository.description }}</p>
     <NuxtLink :href="props.repository.html_url">
-      <NuxtImg sizes="24px" class="icon" src="/img/github_icon.png" alt="Github Icon"/>
+      <NuxtImg width="24px" height="24px" class="icon" src="/img/github_icon.png" alt="Github Icon"/>
     </NuxtLink>
   </div>
 </template>
